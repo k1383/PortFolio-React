9fb01eb55765766040cf803949a5310034ddd424
k1383 <kantin1383@gmail.com>
2025-08-20 14:34:01 +0200

    firts commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import Navbar from "../components/Navbar"
+
+const Home = () => {
+    return (
+        <>
+            <header>
+                <Navbar />
+            </header>
+
+            <main>
+
+            </main>
+
+        </>
+    )
+};
+
+export default Home;
