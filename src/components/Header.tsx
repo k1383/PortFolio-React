b4ba3804fdf043e7020824ced921dd0ab22d2476
k1383 <kantin1383@gmail.com>
2025-08-25 16:53:22 +0200

    typescript

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,26 @@
 import { Link } from 'react-router-dom';
+// import {useEffect, useState } from 'react';
 import '../style/Header.scss'
 
 // Icons
 import { CiMenuFries } from "react-icons/ci"; // BugerMenu  <CiMenuFries />
 
+const burgermenu = (e: React.MouseEvent<HTMLButtonElement>) => {
+	console.log(nav);
+};
+
 const Header = () => (
     
     <header className="container">
         <h1>Cournut Kantin</h1>
-        <nav>
+        <span onClick={burgermenu}><CiMenuFries id='Bugermenu'/></span>    
+        {/* <CiMenuFries id='Bugermenu'/> */}
+        <nav id='nav'>
             <Link to="/">Accueil</Link>
             <Link to="/About">À propos</Link>
             <Link to="/Projects">Projets</Link>
             <Link to="/Contact">Contact</Link>
         </nav>
-        <CiMenuFries id='Bugermenu'/>
     </header>
    
 );
